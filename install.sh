0cd0f04b795489caed931130af9f8dc8da451ade
xfu <xfu@vmware.com>
2014-07-03 15:23:00 +0800

    Add scripts for rest api testing running on jenkins.

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
-
+WORKSPACE=/var/lib/jenkins/workspace/SmokeN-RESTAPI-Apache
 QE_UNTAR_NAME="aurora-qe-test"
-PYTHON_CODE_PATH="${WORKSPACE}/src/testcode
+PYTHON_CODE_PATH="${WORKSPACE}/src/testcode"
 #Set python path
 export PYTHONPATH=${WORKSPACE}
 
 #Get serengeti server ip and vc username/password from aurora-bdc-connection.json
 cd ${WORKSPACE}/${QE_UNTAR_NAME}
-pwd
-SERENGETI_SERVER_IP = 'cat aurora-bdc-connection.json |grep systemUrl |cut -d '"' -f4 |cut -d ':' -f1'
+
+SERENGETI_SERVER_IP=`cat aurora-bdc-connection.json |grep systemUrl |cut -d '"' -f4 |cut -d ':' -f1`
 cd $PYTHON_CODE_PATH/common/
 pwd
-sed -i 's/SERENGETI_SERVER_IP=.*/SERENGETI_SERVER_IP=10.111.57.91/g' $PYTHON_CODE_PATH/common/constants.py
+sed -i "s/SERENGETI_SERVER_IP=.*/SERENGETI_SERVER_IP='${SERENGETI_SERVER_IP}'/g" $PYTHON_CODE_PATH/common/constants.py
 #VC_USERNAME should be base64 encoded.
 #VC_USERNAME =
 #VC_PASSWORD =
