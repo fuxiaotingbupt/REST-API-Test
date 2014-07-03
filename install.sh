219583803e41701a4a7aee3866768949a2d3079b
xfu <xfu@vmware.com>
2014-07-03 10:51:13 +0800

    Add scripts for rest api testing running on jenkins.

---
+++ install.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+set -e
+
+QE_UNTAR_NAME="aurora-qe-test"
+PYTHON_CODE_PATH="${WORKSPACE}/src/testcode
+#Set python path
+export PYTHONPATH=${WORKSPACE}
+
+#Get serengeti server ip and vc username/password from aurora-bdc-connection.json
+cd ${WORKSPACE}/${QE_UNTAR_NAME}
+SERENGETI_SERVER_IP = 'cat aurora-bdc-connection.json |grep systemUrl |cut -d '"' -f4 |cut -d ':' -f1'
+sed -i 's/SERENGETI_SERVER_IP=.*/SERENGETI_SERVER_IP=$SERENGETI_SERVER_IP/g' $PYTHON_CODE_PATH/common/Constants.py
+#VC_USERNAME should be base64 encoded.
+#VC_USERNAME =
+#VC_PASSWORD =
+
+#Execute rest api test cases
+python $PYTHON_CODE_PATH/AutoTest.py
